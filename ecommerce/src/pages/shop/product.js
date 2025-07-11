04341891b65eb60cffef6846330a9adf3087e42b
VarunRaj004 <poohbalarajavarun@gmail.com>
2025-07-11 22:23:07 +0530

    Added cart functionality

--- ecommerce/src/pages/shop/product.js
+++ ecommerce/src/pages/shop/product.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./shop.css"
+import { ShopContext } from "../../context/shopcontext";
+import { useContext } from "react";
+
 
 export const Product = (props) => {
+    const {cart,addToCart} = useContext(ShopContext);
+
     const {id, name, price , image} = props.data;
+
+    const cartAmount = cart[id];
+
     return (
         <div className="product">
             <img src={image}  />
@@ -10,7 +18,7 @@ export const Product = (props) => {
                 <h3 className = "shopTitle">{name}</h3>
                 <p>${price}</p>
             </div>
-          <button className = "addToCartButtn">Add to Card</button>
+          <button className = "addToCartButtn" onClick={() => addToCart(id)}>Add to Cart {cartAmount > 0 && <>({cartAmount})</>}</button>
         </div>
     )
 }
