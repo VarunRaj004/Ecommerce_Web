7e399834558e558fc3206779197a1becd107ef4f
VarunRaj004 <poohbalarajavarun@gmail.com>
2025-07-23 15:11:17 +0530

    Added Profile , LogOut

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -5,7 +5,35 @@ exports.login = async (req, res) => {
     const user = await User.findOne({email});
     if(!user || user.password !== password)
         return res.status(401).send({error:'Invalid Credentials'});
-
+    
+    req.session.userId = user._id;
+    console.log(req.session.userId );
     res.send({message:'Login Successful'})
+
+}
+
+exports.register = async (req,res) => {
+    const {name,email,password} = req.body;
+    const existingUser = await User.findOne({email}) ;
+    if(existingUser)
+        return res.status(400).send({message:'User Already Exists'});
+    const user = User.create({name,email,password});
+    res.send({message:"User Registered"});
+}
+
+exports.profile = async (req,res) => {
+    if(!req.session.userId) return res.status(401).send({message:"Unauthorized Access"});
+    const user = await User.findById(req.session.userId).select('-password');
+    if(!user) return res.status(404).send({message:"User Not Found"});
+
+    res.send(user);
+}
+
+
+exports.logout = (req,res) => {
+    req.session.destroy(err => {
+        if(err) return res.status(500).send({message: "Login Failed"});
+        res.clearCookie('connect.sid').send({message: "Logout Successful"}); 
+    } )
 
 }
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../controller/authController');
 
-router.post('/login',auth.login)
+
+//Post
+router.post('/login',auth.login);
+router.post('/register',auth.register);
+router.post('/logout',auth.logout);
+
+
+//Get
+router.get('/profile',auth.profile)
 
 
 module.exports  = router;
